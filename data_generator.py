45082b9a94ec30fe9ec3863eb17622f7bbcb8adb
Sulaiman <jan.sulaiman@1nce.com>
2019-04-23 10:33:20 +0200

    updates

--- data_generator.py
+++ data_generator.py
@@ -1,5 +1,6 @@
 from faker import Faker
 from faker.providers import internet
+import time
 
 fake = Faker()
 fake.add_provider(internet)
@@ -16,3 +17,20 @@ request = {
 
 print(request)
 
+
+@staticmethod
+def get_kinesis_record():
+    """
+    Generate an item with a random hash key on a large range, and a unique sort key, and  a created date
+    """
+    item = {'hashKey': randrange(0, 5000000), 'sortKey': str(uuid.uuid4()), 'created': datetime.datetime.utcnow().isoformat()}
+    raw_data = json.dumps(item)
+    encoded_data = bytes(raw_data)
+    kinesis_record = {
+        'Data': encoded_data,
+        'PartitionKey': str(item['hashKey']),
+    }
+
+    return kinesis_record
+
+print(get_kinesis_record)
--- producer.py
+++ producer.py
@@ -2,6 +2,8 @@ from kiner.producer import KinesisProducer
 from faker import Faker
 from faker.providers import internet
 
+import time
+
 p = KinesisProducer('testing-stream-2', batch_size=500, max_retries=5, threads=25)
 
 fake = Faker()
@@ -17,40 +19,20 @@ def get_kinesis_record():
     """
     Generate an item with a random hash key on a large range, and a unique sort key, and  a created date
     """
-    item = {'hashKey': randrange(0, 5000000), 'sortKey': str(uuid.uuid4()), 'created': datetime.datetime.utcnow().isoformat()}
+    item = {"hashKey": randrange(0, 5000000), "sortKey": str(uuid.uuid4()), "created": datetime.datetime.utcnow().isoformat()}
     raw_data = json.dumps(item)
     encoded_data = bytes(raw_data)
     kinesis_record = {
-        'Data': encoded_data,
-        'PartitionKey': str(item['hashKey']),
+        "Data": encoded_data,
+        "PartitionKey": str(item["hashKey"]),
     }
 
     return kinesis_record
-
-def generate_and_submit(self):
-    counter = 0
-    # Simple cutoff here - guaranteed to not send in more than maximum_records, with single batch granularity
-    while counter < self.maximum_records and counter < self.maximum_records - self.batch_size:
-        records_batch = [self.get_kinesis_record() for _ in xrange(0, self.batch_size)]
-        request = {
-            'Records': records_batch,
-            'StreamName': 'workflow_data_stream'
-        }
-
-        response = self.kinesis_client.put_records(**request)
-        self.submit_batch_until_successful(records_batch, response)
-
-        counter += len(records_batch)
-        print('Batch inserted. Total records: {}'.format(str(counter)))
-
-    return
+request = {"IP Adresse": fake.ipv4_private(), "Country": fake.word(ext_word_list=my_country)}
 
 #use 60000 for simulation of about 1000 messages per second
-for i in range(10):
-    request = {
-        "IP Adresse": fake.ipv4_private(),
-        "Country": fake.word(ext_word_list=my_country)
-        }
+for i in range(1):
+    request = {"IP Adresse": fake.ipv4_private(), "Country": fake.word(ext_word_list=my_country)}
     p.put_record(request)
     print(request)
 
